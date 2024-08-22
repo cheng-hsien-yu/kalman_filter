a19afdaa0041248394abc72fba8d01d959e69f20
cheng-hsien-yu <cheng-hsien.en13@nycu.edu.tw>
2024-08-22 11:05:00 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/hsien/NCRL/filter_pratice/src
+# Build directory: /home/hsien/NCRL/filter_pratice/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("kalman_filter")
+subdirs("turtlesim_control")
---
+++ build/kalman_filter/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;geometry_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lkalman_filter".split(';') if "-lkalman_filter" != "" else []
+PROJECT_NAME = "kalman_filter"
+PROJECT_SPACE_DIR = "/home/hsien/NCRL/filter_pratice/install"
+PROJECT_VERSION = "0.0.0"
